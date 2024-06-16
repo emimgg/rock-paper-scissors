5aa1f14d4936325077a33185418ddf10110417fd
Emiliano G <ultralightened@gmail.com>
2024-06-16 12:41:40 -0400

    Add logic to compute userChoice and evaluate
    Added if statements to compute the userChoice and
    evaluate it against the computer choice.

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ const ROCK = "ROCK";
 const PAPER = "PAPER";
 const SCISSORS = "SCISSORS";
 
-let userInput = prompt("Choose between rock, paper or scissors")
+let userInput = prompt("Choose between rock, paper or scissors");
+let userChoice = userInput.toLocaleUpperCase();
 let computerAnswer = Math.floor(Math.random() * 3) + 1;
 let enemyChoice;
+
+// COMPUTER CHOICE
 
 if (computerAnswer === 1) {
     enemyChoice = ROCK;
@@ -15,4 +18,34 @@ if (computerAnswer === 1) {
     enemyChoice = SCISSORS;
 }
 
-console.log(enemyChoice);
+// USER CHOICE
+
+if (userChoice === ROCK) {
+    if (enemyChoice === PAPER) {
+        console.log("You've lost!");
+    } else if (enemyChoice === SCISSORS) {
+        console.log("You've won!");
+    } else {
+        console.log("Draw!");
+    }
+}
+
+ if (userChoice === PAPER) {
+    if (enemyChoice === SCISSORS) {
+        console.log("You've lost!");
+    } else if (enemyChoice === ROCK) {
+        console.log("You've won!");
+    } else {
+        console.log("Draw!");
+    }
+ }
+
+ if (userChoice === SCISSORS) {
+    if (enemyChoice === ROCK) {
+        console.log("You've lost!");
+    } else if (enemyChoice === PAPER) {
+        console.log("You've won!");
+    } else {
+        console.log("Draw!")
+    }
+ }
