f281edf512ccdc6dc3db802208310059777b5ea1
Emiliano G <ultralightened@gmail.com>
2024-06-16 13:33:00 -0400

    Declare variables for score keeping

--- script.js
+++ script.js
@@ -2,51 +2,13 @@
 const ROCK = "ROCK";
 const PAPER = "PAPER";
 const SCISSORS = "SCISSORS";
+let humanScore = 0;
+let computerScore = 0;
 
-// let userInput = prompt("Choose between rock, paper or scissors");
-// let userChoice = userInput.toLocaleUpperCase();
 
-// COMPUTER CHOICE
 
-// if (computerAnswer === 1) {
-//     enemyChoice = ROCK;
-// } else if (computerAnswer === 2) {
-//     enemyChoice = PAPER;
-// } else {
-//     enemyChoice = SCISSORS;
-// }
 
-// USER CHOICE
 
-// if (userChoice === ROCK) {
-//     if (enemyChoice === PAPER) {
-//         console.log("You've lost!");
-//     } else if (enemyChoice === SCISSORS) {
-//         console.log("You've won!");
-//     } else {
-//         console.log("Draw!");
-//     }
-// }
-
-//  if (userChoice === PAPER) {
-//     if (enemyChoice === SCISSORS) {
-//         console.log("You've lost!");
-//     } else if (enemyChoice === ROCK) {
-//         console.log("You've won!");
-//     } else {
-//         console.log("Draw!");
-//     }
-//  }
-
-//  if (userChoice === SCISSORS) {
-//     if (enemyChoice === ROCK) {
-//         console.log("You've lost!");
-//     } else if (enemyChoice === PAPER) {
-//         console.log("You've won!");
-//     } else {
-//         console.log("Draw!")
-//     }
-//  }
 
  function getComputerChoice(pcChoice) {
 
