6044bedb5d15c08683286e4d71b9821637a4f9bc
Emiliano G <ultralightened@gmail.com>
2024-06-18 18:07:46 -0400

    Rework code for button input selection
    Removed previous getHumanChoice and added
    button selection instead of text input, for
    better experience

--- script.js
+++ script.js
@@ -1,85 +1,84 @@
-
-const ROCK = "ROCK";
-const PAPER = "PAPER";
-const SCISSORS = "SCISSORS";
+const buttons = document.querySelector(".choices");
+const rock = document.querySelector("#rock-btn");
+const paper = document.querySelector("#paper-btn");
+const scissors = document.querySelector("#scissors-btn");
 let humanScore = 0;
 let computerScore = 0;
 
 
+rock.addEventListener("click", () => playRound("rock", getComputerChoice()));
+paper.addEventListener("click", () => playRound("paper", getComputerChoice()));
+scissors.addEventListener("click", () => playRound("scissors", getComputerChoice()));
 
-// playGame();
+// function playGame() {
+//     for (i = 0; i <5; i++) {
+//         const humanSelection = getHumanChoice();
+//         const computerSelection = getComputerChoice();
 
+//         playRound(humanSelection, computerSelection);
 
-function playGame() {
-    for (i = 0; i <5; i++) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-
-        playRound(humanSelection, computerSelection);
-
-        console.log(humanScore + " points for you!");
-        console.log(computerScore + " points for the computer!");
-    }
-    console.log("FINAL SCORE: \n YOU: " + humanScore + " vs COMPUTER: " + computerScore + "\n GOOD GAME!")
-}
+//         console.log(humanScore + " points for you!");
+//         console.log(computerScore + " points for the computer!");
+//     }
+//     console.log("FINAL SCORE: \n YOU: " + humanScore + " vs COMPUTER: " + computerScore + "\n GOOD GAME!")
+// }
 
 
 
 function playRound(humanChoice, computerChoice) {
-    
-    console.log("You chose: " + humanChoice);
-    console.log("The computer chose: " + computerChoice);
 
     if (humanChoice === computerChoice) {
         console.log("It's a draw!");
     }
 
-    if (humanChoice === PAPER) {
-        if (computerChoice === ROCK) {
+    if (humanChoice === "paper") {
+        if (computerChoice === "rock") {
             console.log("You won! " + humanChoice + " beats " + computerChoice);
             ++humanScore;
-        } else if (computerChoice === SCISSORS) {
+        } else if (computerChoice === "scissors") {
             console.log("You lost! " + computerChoice + " beats " + humanChoice);
             ++computerScore;
         } 
     }
 
-    if (humanChoice === ROCK) {
-        if (computerChoice === SCISSORS) {
+    if (humanChoice === "rock") {
+        if (computerChoice === "scissors") {
             console.log("You won! " + humanChoice + " beats " + computerChoice);
             ++humanScore;
-        } else if (computerChoice === PAPER) {
+        } else if (computerChoice === "paper") {
             console.log("You lost! " + computerChoice + " beats " + humanChoice);
             ++computerScore;
         }
     }
 
-    if (humanChoice === SCISSORS) {
-        if (computerChoice === PAPER) {
+    if (humanChoice === "scissors") {
+        if (computerChoice === "paper") {
             console.log("You won! " + humanChoice + " beats " + computerChoice);
             ++humanScore;
-        } else if (computerChoice === ROCK) {
+        } else if (computerChoice === "rock") {
             console.log("You lost! " + computerChoice + " beats " + humanChoice);
             ++computerScore;
         } 
     }
+    console.log("You chose: " + humanChoice);
+    console.log("The computer chose: " + computerChoice);
 }
 
-function getComputerChoice() {
+function getComputerChoice(computerChoice) {
 
     let num = Math.floor(Math.random() * 3) + 1;
+    let choice = "";
 
-    if (num === 1) {
-        return ROCK;
-    } else if (num === 2) {
-        return PAPER;
-    } else {
-        return SCISSORS;
+    switch (num) {
+        case 1: choice = "rock";
+        break;
+        case 2: choice = "paper";
+        break;
+        case 3: choice = "scissors";
+        break;
     }
+    return choice;
 }
 
-// function getHumanChoice() {
 
-//     let humanChoice = prompt("Choose between rock, paper or scissors").toUpperCase();
-//     return humanChoice;
-// }
+
